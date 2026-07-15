1f74d18d912a4e0c2e5d7b8d2b59ed464b9e54d0
Ravindra Nath Thakur <ravindraq537@gmail.com>
2026-07-15 20:45:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.02%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sumOdd = 0;
+        int sumEven = 0;
+        for(int i=0;i<2*n;i++){
+            if(i%2==0){
+                sumEven = sumEven+i;
+            }            
+        }
+        for(int i=0;i<2*n;i++){
+            if(i%2!=0){
+                sumOdd = sumOdd+i;            }
+        }
+        while(sumEven!=0){
+            int temp = sumEven;
+            sumEven = sumOdd%sumEven;
+            sumOdd = temp;
+        
+        }
+        return sumOdd;
+        
+    }
+};
